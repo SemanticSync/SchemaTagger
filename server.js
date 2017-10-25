89b157e7ff9c46596463b4b0370dc033aa80a72c
chops318 <chops318@gmail.com>
2017-10-24 17:13:57 -0700

    added express

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const morgan = require('morgan');
+
+app.use(morgan('tiny'));
+
+app.use(express.static(`${__dirname}/build`));
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/build/index.html`);
+});
+
+app.listen(port, () => {
+  console.log('server up ::', port);
+});
