10162f51e47c9f21b3895347689751fc7f307854
chops318 <chops318@gmail.com>
2017-10-24 18:46:24 -0700

    cors added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 8080;
 const morgan = require('morgan');
 
